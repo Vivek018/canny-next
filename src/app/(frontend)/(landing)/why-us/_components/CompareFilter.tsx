de83aa1db559396945271c69fb741d39adc35202
Vivek <vc1262461@gmail.com>
2023-08-16 11:10:34 +0530

    Done

--- src/app/(frontend)/(landing)/why-us/_components/CompareFilter.tsx
+++ src/app/(frontend)/(landing)/why-us/_components/CompareFilter.tsx
@@ -15,6 +15,7 @@ export function CompareFilter({ index, setIndex }: Props) {
     <div className='fixed bottom-10 top-1/2 md:top-auto left-1/2 transform -translate-x-1/2  -translate-y-1/2 md:-translate-y-0 flex  gap-20 xs:gap-28 sm:gap-40 md:gap-4 justify-between items-center z-50'>
       <Button
         variant='ghost'
+        role='slider'
         size='icon'
         disabled={index === 1}
         className='bg-primary md:bg-transparent text-background md:text-foreground w-12 h-12 sm:w-16 sm:h-16 md:w-12 md:h-12 hover:bg-primary md:hover:bg-accent md:disabled:hover:bg-transparent shadow-lg md:shadow-none'
@@ -27,6 +28,7 @@ export function CompareFilter({ index, setIndex }: Props) {
           <Button
             key={val}
             variant='ghost'
+            role='slider'
             className={cn(
               "hidden md:flex w-8 h-8 rounded-md",
               index === val && "cursor-auto bg-accent"
@@ -40,6 +42,7 @@ export function CompareFilter({ index, setIndex }: Props) {
       <Button
         variant='ghost'
         size='icon'
+        role='slider'
         disabled={index === 6}
         className='bg-primary md:bg-transparent text-background md:text-foreground w-12 h-12 sm:w-16 sm:h-16 md:w-12 md:h-12 hover:bg-primary md:hover:bg-accent md:disabled:hover:bg-transparent shadow-lg md:shadow-none'
         onClick={() => setIndex((prevValue) => prevValue + 1)}
