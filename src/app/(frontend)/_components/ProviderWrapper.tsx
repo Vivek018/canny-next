305614a4f8d01d74abde436eba5f62d65cc8da74
Vivek <vc1262461@gmail.com>
2023-08-21 15:14:24 +0530

    Done

--- src/app/(frontend)/_components/ProviderWrapper.tsx
+++ src/app/(frontend)/_components/ProviderWrapper.tsx
@@ -2,7 +2,13 @@
 
 import { ThemeProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { Analytics } from "@vercel/analytics/react";
 
 export function ProviderWrapper({ children, ...props }: ThemeProviderProps) {
-  return <ThemeProvider {...props}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider {...props}>
+      {children}
+      <Analytics />
+    </ThemeProvider>
+  );
 }
